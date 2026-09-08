524647560b70207d68d790ce612bb4c8699acba1
Nguyen Son Chu <email-cua-ban@gmail.com>
2026-09-08 14:43:14 +0700

    Nop bai thuc hanh 1 - Nguyen Son Chu

---
+++ thanhvien/sonchu/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Xin chào PHP</title>
+</head>
+<body>
+    <h1><?php echo "Hello World from PHP!"; ?></h1>
+</body>
+</html>
