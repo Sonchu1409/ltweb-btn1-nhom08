331ab5b432bdb32ba5fd26ce7def20537e61c319
Nguyen Son Chu <email-cua-ban@gmail.com>
2026-09-11 20:53:13 +0700

    Fix date format in hello.php

--- thanhvien/sonchu/hello.php
+++ thanhvien/sonchu/hello.php
@@ -2,5 +2,5 @@
 // hello.php – trang PHP đầu tiên
 $monhoc = "Thiết kế và Lập trình web";
 echo "<h1>Xin chào $monhoc!</h1>";
-echo "<p>Bây giờ là " . date("H:i, ngày d/m/Y") . " trên server.</p>";
+echo "<p>Bây giờ là " . date("H:i") . ", ngày " . date("d/m/Y") . " trên server.</p>";
 phpinfo();
